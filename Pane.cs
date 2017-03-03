37171eb237fd67d1a6671c72f21ded35cc4c79f0
Tim Davis <tim.hifi@gmail.com>
2017-03-02 21:21:50 -0600

    Moved the bits of the UI out of the Pane class.

--- Pane.cs
+++ Pane.cs
@@ -1,32 +1,16 @@
-﻿using System.IO;
-using System.Collections.Generic;
-
-namespace mc
+﻿namespace mc
 {
     class Pane : UIElement
     {
-        private TextList FileList;
-        private TextBox CurrentLocation;
-        private const int CurrentLocationHeight = 2;
+
 
         public Pane(int PosX, int PosY, int PaneWidth, int PaneHeight, string Location) : base(PosX, PosY, PaneWidth, PaneHeight)
         {
             DrawBorder();
-
-            FileList = new TextList(PosX + 1, PosY + 1, PaneWidth - 1, PaneHeight - 2, new List<string>(Directory.GetFiles(Location)));
-            FileList.DrawContents();
-            CurrentLocation = new TextBox(base.X, base.Y + PaneHeight - CurrentLocationHeight, PaneWidth, CurrentLocationHeight);
-            CurrentLocation.DrawBorder(true, false, false, false);
-        }
-
-        public void SetCurrentLocation(string NewLocation)
-        {
-            CurrentLocation.Text = NewLocation;
         }
 
         public override void DrawContents()
         {
-            CurrentLocation.DrawContents();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace mc
 {
     class Program
     {
+        const int CurrentLocationHeight = 2;
+        const int LeftPanePosX = 0;
+        const int LeftPanePosY = 0;
+        const int LeftPaneWidth = 39;
+        const int LeftPaneHeight = 29;
+
+        const int RightPanePosX = 40;
+        const int RightPanePosY = 0;
+        const int RightPaneWidth = 39;
+        const int RightPaneHeight = 29;
+
+        static Pane LeftPane;
+        static TextList LeftFileList;
+        static TextBox LeftCurrentLocation;
+        static Pane RightPane;
+        static TextList RightFileList;
+        static TextBox RightCurrentLocation;
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.Clear();
 
-            string HomeDirectory = Environment.GetEnvironmentVariable("HOME");
+            BuildUI();
 
-            Pane LeftPane = new Pane(0, 0, 39, 29, HomeDirectory);
-            Pane RightPane = new Pane(40, 0, 39, 29, HomeDirectory);
-
-            RightPane.SetCurrentLocation(HomeDirectory);
-            LeftPane.SetCurrentLocation(HomeDirectory);
+            List<UIElement> UIElements = new List<UIElement>();
+            UIElements.Add(LeftPane);
+            UIElements.Add(RightPane);
 
             LeftPane.DrawContents();
             RightPane.DrawContents();
 
-            Console.ReadKey();
+            ConsoleKeyInfo ReadKey;
+            do
+            {
+                ReadKey = Console.ReadKey();
+                switch (ReadKey.Key)
+                {
+                    case ConsoleKey.Tab:
+                        if ((ReadKey.Modifiers & ConsoleModifiers.Shift) != 0)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (ReadKey.Key != ConsoleKey.Escape);
+        }
+
+        private static void BuildUI()
+        {
+            string HomeDirectory = Environment.GetEnvironmentVariable("HOME");
+
+            LeftPane = new Pane(LeftPanePosX,
+                                LeftPanePosY,
+                                LeftPaneWidth,
+                                LeftPaneHeight,
+                                HomeDirectory);
+
+            RightPane = new Pane(RightPanePosX,
+                                 RightPanePosY,
+                                 RightPaneWidth,
+                                 RightPaneHeight,
+                                 HomeDirectory);
+
+            LeftFileList = new TextList(LeftPanePosX + 1,
+                                        LeftPanePosY + 1,
+                                        LeftPaneWidth - 1,
+                                        LeftPaneHeight - 2,
+                                        new List<string>(Directory.GetFiles(HomeDirectory)));
+
+            LeftCurrentLocation = new TextBox(LeftPanePosX,
+                                              LeftPanePosY + LeftPaneHeight - CurrentLocationHeight,
+                                              LeftPaneWidth,
+                                              CurrentLocationHeight);
+
+            RightFileList = new TextList(RightPanePosX + 1,
+                                         RightPanePosY + 1,
+                                         RightPaneWidth - 1,
+                                         RightPaneHeight - 2,
+                                         new List<string>(Directory.GetFiles(HomeDirectory)));
+
+            RightCurrentLocation = new TextBox(RightPanePosX,
+                                               RightPanePosY + RightPaneHeight - CurrentLocationHeight,
+                                               RightPaneWidth,
+                                               CurrentLocationHeight);
+            LeftFileList.DrawContents();
+            LeftCurrentLocation.DrawBorder(true, false, false, false);
+            LeftCurrentLocation.Text = HomeDirectory;
+
+            RightFileList.DrawContents();
+            RightCurrentLocation.DrawBorder(true, false, false, false);
+            RightCurrentLocation.Text = HomeDirectory;
         }
     }
 
